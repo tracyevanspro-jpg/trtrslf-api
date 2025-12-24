95e6e76617b9fc44259724ec990dc041f405a63f
exhibitry <tracy@tracyevansproductions.com>
2025-12-24 17:52:11 -0500

    Add puzzle categories

--- api/puzzle.js
+++ api/puzzle.js
@@ -1,11 +1,42 @@
+const PUZZLES = {
+  biblical: [
+    { phrase: "IN THE BEGINNING", source: "Genesis 1:1" },
+    { phrase: "LET THERE BE LIGHT", source: "Genesis 1:3" }
+  ],
+  movies: [
+    { phrase: "MAY THE FORCE BE WITH YOU", source: "Star Wars" },
+    { phrase: "HERE IS LOOKING AT YOU KID", source: "Casablanca" }
+  ],
+  pop: [
+    { phrase: "JUST DO IT", source: "Nike" },
+    { phrase: "I WANT MY MTV", source: "MTV" }
+  ],
+  songs: [
+    { phrase: "IMAGINE ALL THE PEOPLE", source: "John Lennon" },
+    { phrase: "WE WILL ROCK YOU", source: "Queen" }
+  ]
+};
+
+function stripVowels(str) {
+  return str.replace(/[AEIOU]/gi, "");
+}
+
+function stripSpaces(str) {
+  return str.replace(/\s+/g, "");
+}
+
 export default function handler(req, res) {
+  const category = (req.query.category || "biblical").toLowerCase();
+  const list = PUZZLES[category] || PUZZLES.biblical;
+  const item = list[Math.floor(Math.random() * list.length)];
+
   res.status(200).json({
-    phrase: "IN THE BEGINNING",
-    noVowels: "N TH BGNNNG",
-    noSpaces: "INTHEBEGINNING",
+    phrase: item.phrase,
+    noVowels: stripVowels(item.phrase),
+    noSpaces: stripSpaces(item.phrase),
     meta: {
-      source: "Genesis 1:1",
-      genre: "Biblical 2"
+      source: item.source,
+      category
     }
   });
 }
