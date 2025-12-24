5f79cfa5f052a4b13497406958644724c306a3d8
tracyevanspro-jpg <tracyevanspro@gmail.com>
2025-12-24 14:15:41 -0500

    Create puzzle.js

---
+++ api/puzzle.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    phrase: "IN THE BEGINNING",
+    noVowels: "N TH BGNNNG",
+    noSpaces: "INTHEBEGINNING",
+    meta: {
+      source: "Genesis 1:1",
+      genre: "Biblical"
+    }
+  });
+}
