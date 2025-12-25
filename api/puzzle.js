a9318284e4462f49e87f94cdb3465007a2b267be
tracyevanspro-jpg <tracyevanspro@gmail.com>
2025-12-25 13:07:18 -0500

    Update puzzle.js

--- api/puzzle.js
+++ api/puzzle.js
@@ -1,42 +1,15 @@
-const PUZZLES = {
-  biblical: [
-    { phrase: "IN THE BEGINNING", source: "Genesis 1:1" },
-    { phrase: "LET THERE BE LIGHT", source: "Genesis 1:3" }
-  ],
-  movies: [
-    { phrase: "MAY THE FORCE BE WITH YOU", source: "Star Wars" },
-    { phrase: "HERE IS LOOKING AT YOU KID", source: "Casablanca" }
-  ],
-  pop: [
-    { phrase: "JUST DO IT", source: "Nike" },
-    { phrase: "I WANT MY MTV", source: "MTV" }
-  ],
-  songs: [
-    { phrase: "IMAGINE ALL THE PEOPLE", source: "John Lennon" },
-    { phrase: "WE WILL ROCK YOU", source: "Queen" }
-  ]
-};
-
-function stripVowels(str) {
-  return str.replace(/[AEIOU]/gi, "");
-}
-
-function stripSpaces(str) {
-  return str.replace(/\s+/g, "");
-}
-
 export default function handler(req, res) {
-  const category = (req.query.category || "biblical").toLowerCase();
-  const list = PUZZLES[category] || PUZZLES.biblical;
-  const item = list[Math.floor(Math.random() * list.length)];
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-  res.status(200).json({
-    phrase: item.phrase,
-    noVowels: stripVowels(item.phrase),
-    noSpaces: stripSpaces(item.phrase),
+  const puzzle = {
+    phrase: "LET THERE BE LIGHT",
+    noVowels: "LT THR B LGHT",
+    noSpaces: "LETTHEREBELIGHT",
     meta: {
-      source: item.source,
-      category
+      source: "Genesis 1:3",
+      category: "biblical"
     }
-  });
+  };
+
+  res.status(200).json(puzzle);
 }
